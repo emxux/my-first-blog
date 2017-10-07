d1f327a71b6985557b0760669890f4fe26de559e
Emilia <emilia.xue@gmail.com>
2017-10-07 12:19:26 +0300

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+
+name= "Emilia"
+print("Hello world " + name)
+print("Lenght of name Emilia " + "''" + str(len(name)) + "''")
+{}
+participant ={'name': 'Emilia', 'country':'Finland','favourite_numbers': [1,2,3]}
+print(participant)
+print(participant['name'])
